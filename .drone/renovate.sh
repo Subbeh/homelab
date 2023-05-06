b2f6191553c8fe806f27cf472b2f712243ef28cd
Steven <steven001@gmail.com>
2023-05-06 23:25:25 +1000

    fix(drone): fix renovate script

--- .drone/renovate.sh
+++ .drone/renovate.sh
@@ -2,7 +2,7 @@
 set -x
 set -eo pipefail
 
-CONTAINER_PLAYBOOK=playbooks/container.yml
+APP_PLAYBOOK=playbooks/app.yml
 
 git config --global --add safe.directory /drone/src
 echo "${ANSIBLE_VAULT_PASSWORD:?not set}" > $HOME/.ansible_vault
@@ -12,9 +12,9 @@ cd ansible
 
 while read file ; do
   echo checking file $file...
-  if [[ "$file" == ansible/containers/*.yml ]] ; then
-    container=$(echo "$file" | sed 's#.*/\(.\+\).ya\?ml#\1#')
-    echo running container script for $container...
-    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "${CONTAINER_PLAYBOOK:?not set}" --tags $container
+  if [[ "$file" == ansible/apps/*.yml ]] ; then
+    app=$(echo "$file" | sed 's#.*/\(.\+\).ya\?ml#\1#')
+    echo running app script for $app...
+    ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "${APP_PLAYBOOK:?not set}" --tags $app
   fi
 done <<< $(git diff --name-only --diff-filter=ACMR HEAD~1 main)
