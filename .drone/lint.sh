158776dd32e5ac8472f32d22713ed5d0198cbe1d
Steven <steven001@gmail.com>
2023-05-07 23:20:05 +1000

    feat(drone): enable ansible-lint

--- .drone/lint.sh
+++ .drone/lint.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
+
 set -x
 set -eo pipefail
 
-# env
-# ssh -o StrictHostKeyChecking=false -T git@github.com || true
-# sleep 10000
-# rm ansible/requirements.yml 2>/dev/null
-
 while read file ; do
   echo checking file $file...
-  # if [[ "$file" =~ ansible/playbooks/.*\.ya?ml ]] ; then
-  #   cd ansible
-  #   ansible-lint -c .ansible-lint "../$file"
-  #   cd -
-  # fi
+  if [[ "$file" =~ ansible/playbooks/.*\.ya?ml ]] ; then
+    cd ansible
+    ansible-lint -c .ansible-lint "../$file"
+    cd -
+  fi
   if [[ "$file" =~ ansible/.*\.ya?ml$ ]] ; then
     yamllint -s -c .yamllint "$file"
   fi
