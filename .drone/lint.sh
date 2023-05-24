06a2ccdf4a0bab4807664787339e360caa1f15ea
Steven <steven001@gmail.com>
2023-05-24 15:04:22 +1000

    feat(ci): add linting to roles and tasks

--- .drone/lint.sh
+++ .drone/lint.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-set -x
 set -eo pipefail
 
 echo "${ANSIBLE_VAULT_PASSWORD:?not set}" > $HOME/.ansible_vault
 
 while read file ; do
   echo checking file $file...
-  if [[ "$file" =~ ansible/playbooks/.*\.ya?ml ]] ; then
+  if [[ "$file" =~ ansible/(roles|playbooks|tasks)/.*\.ya?ml ]] ; then
     cd ansible
-    ansible-lint -v -c .ansible-lint "playbooks/${file##*/}"
+    ansible-lint -v -c .ansible-lint "${file#*/}"
     cd -
   fi
   if [[ "$file" =~ ansible/.*\.ya?ml$ ]] ; then
