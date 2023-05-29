a51e195de6f32fed9887f689c3edefddcc9cd5c3
Steven <steven001@gmail.com>
2023-05-29 21:08:46 +1000

    fix(drone): changes to apps script

--- .drone/apps.sh
+++ .drone/apps.sh
@@ -5,8 +5,10 @@ APP_PLAYBOOK=playbooks/app.yml
 
 git config --global --add safe.directory /drone/src
 echo "${ANSIBLE_VAULT_PASSWORD:?not set}" > $HOME/.ansible_vault
-mkdir -p $HOME/.ssh && echo "${SSH_KEY:?not set}" > $HOME/.ssh/id_ed25519 && chmod -R 600 $HOME/.ssh
-cat $HOME/.ssh/id_ed25519
+if [ ! -r $HOME/.ssh/id_ed25519 ] ; then
+  mkdir -p $HOME/.ssh && echo "${SSH_KEY:?not set}" > $HOME/.ssh/id_ed25519 && chmod -R 600 $HOME/.ssh
+fi
+
 pushd ansible
 
 while read file ; do
@@ -16,4 +18,5 @@ while read file ; do
     echo running app script for $app...
     ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "${APP_PLAYBOOK:?not set}" --tags $app
   fi
-done <<< $(git log --name-only --oneline --diff-filter=ACMR origin/main@{1}..origin/main | awk 'length($1) != 7')
+done <<< $(git diff --name-only --diff-filter=ACMR HEAD~1 main)
+# done <<< $(git log --name-only --oneline --diff-filter=ACMR origin/main@{1}..origin/main | awk 'length($1) != 7')
