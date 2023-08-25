f1f35ae1c49c5625ea12e9f1bfa37c0727bf628d
Steven <steven001@gmail.com>
2023-08-25 16:20:00 +1000

    feat(ci): add APP argument for custom builds

--- .drone/apps.sh
+++ .drone/apps.sh
@@ -3,18 +3,31 @@
 set -xeuo pipefail
 
 APP_PLAYBOOK=playbooks/app.yml
+APP=${APP:-}
 
-pushd ansible
-
-git config --global --add safe.directory /drone/src
-echo "${ANSIBLE_VAULT_PASSWORD:?not set}" >$HOME/.ansible_vault
-if [ ! -r $HOME/.ssh/id_ed25519 ]; then
-	mkdir -p $HOME/.ssh && echo "${DRONE_SSH_KEY:?not set}" >$HOME/.ssh/id_ed25519 && chmod -R 600 $HOME/.ssh
-fi
-
-while read app; do
-	if [[ $app ]]; then
-		echo running app script for $app...
-		ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "${APP_PLAYBOOK:?not set}" -e "app=$app" -e "ci=true"
+run_playbook() {
+	if [[ ${1:-} ]]; then
+		echo running app script for $1...
+		ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "${APP_PLAYBOOK:?not set}" -e "app=$1" -e "ci=true"
 	fi
-done <<<$(git diff --name-only --diff-filter=ACMR HEAD~1 main | grep -oP '(?<=ansible/)apps/[^/]*.ya?ml' | xargs awk -F: '/^[^ -]/{ print $1 }')
+}
+
+main() {
+	pushd ansible
+
+	git config --global --add safe.directory /drone/src
+	echo "${ANSIBLE_VAULT_PASSWORD:?not set}" >$HOME/.ansible_vault
+	if [ ! -r $HOME/.ssh/id_ed25519 ]; then
+		mkdir -p $HOME/.ssh && echo "${DRONE_SSH_KEY:?not set}" >$HOME/.ssh/id_ed25519 && chmod -R 600 $HOME/.ssh
+	fi
+
+	if [[ $APP ]]; then
+		run_playbook $APP
+	else
+		while read app; do
+			run_playbook $app
+		done <<<$(git diff --name-only --diff-filter=ACMR HEAD~1 main | grep -oP '(?<=ansible/)apps/[^/]*.ya?ml' | xargs awk -F: '/^[^ -]/{ print $1 }')
+	fi
+}
+
+main $@
