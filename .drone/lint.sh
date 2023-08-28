50bbf37a62805133f51b8c2d15d804495907de34
Steven <steven001@gmail.com>
2023-08-29 08:45:43 +1000

    feat(ci): add DEBUG arg for drone pipeline

--- .drone/lint.sh
+++ .drone/lint.sh
@@ -3,6 +3,10 @@ set -xeuo pipefail
 
 echo "${ANSIBLE_VAULT_PASSWORD:?not set}" >$HOME/.ansible_vault
 
+if [[ ${DEBUG:-} ]]; then
+	sleep 600
+fi
+
 while read file; do
 	echo checking file $file...
 	if [[ "$file" =~ ansible/(roles|playbooks|tasks)/.*\.ya?ml ]]; then
