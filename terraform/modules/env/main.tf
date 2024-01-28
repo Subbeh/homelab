26661872829217634c00f9288273742a9edb34c6
drone <sysadm@noreply.git.sbbh.cloud>
2024-01-28 13:00:30 +0000

    feat(tf): update terraform doppler from 1.3.0 to v1.4.0

--- terraform/modules/env/main.tf
+++ terraform/modules/env/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     doppler = {
       source  = "DopplerHQ/doppler"
-      version = "1.3.0"
+      version = "1.4.0"
     }
     sops = {
       source  = "carlpett/sops"
--- terraform/pve/providers.tf
+++ terraform/pve/providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     doppler = {
       source  = "DopplerHQ/doppler"
-      version = "1.3.0"
+      version = "1.4.0"
     }
   }
 }
